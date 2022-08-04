e19c7933f96789f9a361dfc15310d23e6f11b59a
sreekanthkumarraju <rvsreekanthkumar90@gmail.com>
2022-08-04 06:59:54 +0530

    first commit

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose')
+
+const Schema=mongoose.Schema
+
+const userSchema=new  Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAvatarImageSet: {
+        type: Boolean,
+        default: false,
+      },
+      avatarImage: {
+        type: String,
+        default: "",
+      },
+})
+
+const userModel=mongoose.model('user',userSchema)
+
+module.exports=userModel
